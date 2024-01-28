acb6c4c2cf1d24c0f384d37d034131cef1d12492
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2024-01-28 11:06:18 -0500

    Create main.c
    laid out bare bones (network config)

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <gccore.h>
+#include <wpad/wiiuse.h>
+#include <ogcsys.h>
+#include <network.h>
+
+void init_network() {
+  if (network_init() < 0) {
+    // error logic here
+  }
+}
+
+void connect() {
+  init_network();
+  if (network_connect() < 0) {
+    // error logic here
+  }
+}
+
+int main() {
+  // main function
+  return 0;
+}
