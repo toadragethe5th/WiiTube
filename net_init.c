63af08bf7f32ed7b8a3398c698d1ae14cb2f5762
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2024-02-01 14:09:28 -0500

    Update and rename network locater.c to net_init.c

---
+++ net_init.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+#include <wiiuse/wpad.h>
+#include <network.h>
+#include <ogc.h>
+
+void init() {
+
+    net_init();
+
+    if (net_init() < 0) {
+
+        printf("E101: Unable to connect to network.\n");
+        
+    }
+    
+}
+
+//
+// YOUTUBE MEDIA DOWNLOADER IS TO BE CODED!
+//
