ba553f3ffb27a94c179742a7d22a2449a5da87e5
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-02-01 11:51:03 -0500

    Update network locater.c

--- network locater.c
+++ network locater.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-
+#include <unistd.h> 
+#include
 
 int networkfound = 0;
 
 
 int main() {
 printf("welcome to wiitube by guinea7pig and toadragethe5th.*\n");
-//wait 
+//wait
 //* some discalamer about how this just a portal to yt and all that boring siclamer stuff
 printf("locating network...\n");
 //code for locating network goes here
+//wait
 if(networkfound==1) {
 printf("network located\n");//return zero
 } else{
