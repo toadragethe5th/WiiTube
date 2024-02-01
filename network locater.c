3db842abd054793a7439deb88c97560e8c61aadf
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-02-01 13:53:53 -0500

    Update network locater.c
    cleaned up the code a nit

--- network locater.c
+++ network locater.c
@@ -2,10 +2,28 @@
 #include <unistd.h> 
 
 
-int networkfound = 0;
+int networkfound = 1;
 
 void color(){
  printf("\033[0;32m");
+}
+
+void checknetwork(){
+  //code for locating network connecion goes here
+}
+
+void load(){
+//code for loadig here 
+}
+
+void fail(){
+printf("error:connecrion failed\n");
+printf("please check your router, connection to your wii, or try riiconecting\n");
+}
+void succsess(){
+printf("network located\n");//return zero
+//wait
+printf("loading app\n");
 }
 
 int main() {
@@ -14,15 +32,12 @@ printf("welcome to wiitube by guinea7pig and toadragethe5th.*\n");
 
 //* some discalamer about how this just a portal to yt and all that boring siclamer stuff
 printf("locating network...\n");
-//code for locating network goes here
-//wait
+checknetwork();
+//wait unitl network found if not after a muinite,go to connection failed and start again
 if(networkfound==1) {
-printf("network located\n");//return zero
-//wait
-printf("loading app\n");
+succsess();
 } else{
-printf("error:connecrion failed\n");
-printf("please check your router, connection to your wii, or try riiconecting\n");
+fail();
 return 0;
 }
 }
