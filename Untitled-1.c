2b832709b8be4a1569397f39dbb48e8d4fb501a6
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-02-01 10:47:49 -0500

    network locater (i guees)

---
+++ Untitled-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+
+int main() {
+printf("welcome to wiitube by guinea7pig and toadragethe5th.*/n");
+//wait 
+//* some discalamer about how this just a portal to yt and all that boring siclamer stuff
+printf("locating network.../n");
+//code for locating network goes here
+printf("network located/n");//return zero
+//wait code goes here
+} 
