fc395846caca394a662a7d430d300ea7deb6c000
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-02-01 10:54:36 -0500

    Update network locater.c

--- network locater.c
+++ network locater.c
@@ -4,13 +4,12 @@
 
 
 int main() {
-printf("welcome to wiitube by guinea7pig and toadragethe5th.*/n");
+printf("welcome to wiitube by guinea7pig and toadragethe5th.*\n");
 //wait 
 //* some discalamer about how this just a portal to yt and all that boring siclamer stuff
-printf("locating network.../n");
+printf("locating network...\n");
 //code for locating network goes here
-printf("network located/n");//return zero
-
-printf("error:connecrion failed/n"); 
+printf("network located\n");//return zero
 //wait code goes here
+printf("error:connecrion failed\n"); 
 } 
