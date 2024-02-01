67e3174fda0f541e1bb94f500685de45a7787eb6
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-02-01 10:52:30 -0500

    Update and rename Untitled-1.c to network locater.c

--- network locater.c
+++ network locater.c
@@ -10,5 +10,7 @@ printf("welcome to wiitube by guinea7pig and toadragethe5th.*/n");
 printf("locating network.../n");
 //code for locating network goes here
 printf("network located/n");//return zero
+
+printf("error:connecrion failed/n"); 
 //wait code goes here
 } 
