5ff6a972d97a035cfd42481553ac6283e0b65574
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-02-02 07:31:02 -0500

    Update net_init.c

--- net_init.c
+++ net_init.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <unistd.h>
-#include <wiiuse/wpad.h>
-#include <network.h>
-#include <ogc.h>
+#include <unistd.h> 
 
-void init() {
 
-    net_init();
+int networkfound = 0;
 
-    if (net_init() < 0) {
-
-        printf("E101: Unable to connect to network.\n");
-        
-    }
-    
+void color(){
+ printf("\033[0;32m");
 }
 
-//
-// YOUTUBE MEDIA DOWNLOADER IS TO BE CODED!
-//
-//:(
+void checknetwork(){
+  //code for locating network connecion goes here
+}
+
+void load(){
+//code for loadig here 
+}
+
+void fail(){
+printf("error:connecrion failed\n");
+printf("please check your router, connection to your wii, or try riiconecting\n");
+}
+void succsess(){
+printf("network located\n");//return zero
+//wait
+printf("loading app\n");
+}
+
+int main() {
+color();  
+printf("welcome to wiitube by guinea7pig and toadragethe5th.*\n");
+
+//* some discalamer about how this just a portal to yt and all that boring siclamer stuff
+printf("locating network...\n");
+checknetwork();
+//wait unitl network found if not after a muinite,go to connection failed and start again
+if(networkfound==1) {
+succsess();
+} else{
+fail();
+return 0;
+}
+}
