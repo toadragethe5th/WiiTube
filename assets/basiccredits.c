edfab72c715c7cac2830d5c9a7dbfac61ce4b313
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-02-02 09:51:19 -0500

    Update basiccredits.c

--- assets/basiccredits.c
+++ assets/basiccredits.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <unistd.h> 
 
@@ -18,6 +17,15 @@ int main() {
     //wait
      printf("                graphic design: (currently) by guiea7pig\n");
     //wait
-     printf("                       sound design: by n/a \n");   
+     printf("                       sound design: by n/a \n");  
+    //wait 
+     printf("                   speacil thanks to the app wii mc \n");
+     printf("             and there creators for the insporation for project \n");   
+     //wait  
+     printf("          specail specail thanks to the wii hombrewing cummuinity     \n");   
+     //wait
+      printf("       check out or project at https://github.com/toadragethe5th/WiiTube    \n");   
+      //return to menu
+                      
 }
     
