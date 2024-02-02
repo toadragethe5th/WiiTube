1a1cc1c0b944a7d485d3c02e04198b2bb7046701
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-02-02 09:42:28 -0500

    Update basiccredits.c

--- assets/basiccredits.c
+++ assets/basiccredits.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+#include <unistd.h> 
+
+
+
+void color() {
+    
+    printf("\033[0;31m");
+     
+}
+int main() {
+    
+    color();  
+    printf("                               wiitube \n");
+    //wait
+    printf("                coding: by toadragethe5th and guiea7pig\n");
+    //wait
+     printf("                graphic design: (currently) by guiea7pig\n");
+    //wait
+     printf("                       sound design: by n/a \n");   
+}
+    
