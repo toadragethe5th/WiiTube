5f0b85f326f13b3329112574bceb06a902a1842d
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-02-02 07:31:22 -0500

    Update net_init.c

--- net_init.c
+++ net_init.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <unistd.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
+#include <wiiuse/wpad.h>
+#include <network.h>
+#include <ogc.h>
 
 
 int networkfound = 0;
