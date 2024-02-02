5fd16b44878a287fafd17b34ba1121e39c21ebdc
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2024-02-02 18:47:55 -0500

    edited credit function, will do header management tommorow

---
+++ src/assets/basiccredits.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h> 
+#include <stdint.h>
+
+int main() {
+    
+    color();  
+    printf("                               WiiTube\n\n");
+    //wait
+    printf("                 coded by toadragethe5th and guinea7pig\n");
+    //wait
+    printf("                    graphic design by guinea7pig\n");
+    //wait
+    printf("                       sound design: by n/a \n");  
+    //wait 
+    printf("                   special thanks to the app wiimc \n");
+    printf("             and their creators for the insporation for project. \n\n");   
+     //wait  
+    printf("            special special thanks to the wii homebrewing community.     \n\n");   
+     //wait
+    printf("        check out our project at https://github.com/toadragethe5th/WiiTube.    \n");   
+    /* return to menu
+    add some credit to yt */
+}
+    
