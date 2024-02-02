c88383e3398235372b9c892f93167e753bfdf62a
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2024-02-02 12:52:45 -0500

    Update net_init.c

--- net_init.c
+++ net_init.c
@@ -8,23 +8,38 @@
 #include <network.h>
 #include <ogc.h>
 
-
-int networkfound = 0;
-
-void color() {
+void Initialise() {
     
-    printf("\033[0;32m");
+    VIDEO_Init();
+    WPAD_Init();
+    rmode = VIDEO_GetPreferredMode(NULL);
+    xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+    console_init(xfb,20,20, rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+    VIDEO_Configure(rmode);
+    VIDEO_SetNextFramebuffer(xfb);
+    VIDEO_SetBlack(FALSE);
+    VIDEO_Flush();
+    VIDEO_WaitVSync();
+    if (rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
     
 }
 
-void connect() {
+
+void init_Connect() {
     
     net_init();
     if net_init() < 0 {
 
-        printf("Error connecting to network.\n");
+        printf("101: Error initializing network.\n");
         
     }
+
+    // 
+    // I believe that this is the correct method for fetching an IP address.
+    int IP = net_gethostip();
+    struct netconn *conn = do_newconn(NETCONN_TCP);
+    u16 port = 80;
+    netconn_connect(conn, IP, port);
     
 }
 
@@ -50,23 +65,19 @@ void success() {
 }
 
 int main() {
-    
-    color();  
+    initialize();
     printf("welcome to wiitube by guinea7pig and toadragethe5th.*\n");
 
-    //* some discalamer about how this just a portal to yt and all that boring siclamer stuff
-    printf("locating network...\n");
-    checknetwork();
-    //wait unitl network found if not after a muinite,go to connection failed and start again
-    if (networkfound==1) {
-        
-        success();
-        
-    } else {
-        
-        fail();
-        
-    return 0;
+    while(1) {
+
+        WPAD_ScanPads();
+        u16 buttonsDown = WPAD_ButtonsDown(0);
+
+        if (buttonsDown & WPAD_BUTTON_HOME) {
+
+            exit(0);
+            
+        }
         
     }
     
