460d6e31f2fa650832c0ccc07dbc82040678ef82
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2024-02-02 07:46:12 -0500

    Update net_init.c

--- net_init.c
+++ net_init.c
@@ -11,40 +11,63 @@
 
 int networkfound = 0;
 
-void color(){
- printf("\033[0;32m");
+void color() {
+    
+    printf("\033[0;32m");
+    
 }
 
-void checknetwork(){
-  //code for locating network connecion goes here
+void connect() {
+    
+    net_init();
+    if net_init() < 0 {
+
+        printf("Error connecting to network.\n");
+        
+    }
+    
 }
 
-void load(){
-//code for loadig here 
+void load() { 
+    
+    //code for loadig here 
+    
 }
 
-void fail(){
-printf("error:connecrion failed\n");
-printf("please check your router, connection to your wii, or try riiconecting\n");
+void fail() {
+    
+    printf("error:connecrion failed\n");
+    printf("please check your router, connection to your wii, or try riiconecting\n");
+    
 }
-void succsess(){
-printf("network located\n");//return zero
-//wait
-printf("loading app\n");
+
+void success() {
+    
+    printf("network located\n");//return zero
+    //wait
+    printf("loading app\n");
+    
 }
 
 int main() {
-color();  
-printf("welcome to wiitube by guinea7pig and toadragethe5th.*\n");
+    
+    color();  
+    printf("welcome to wiitube by guinea7pig and toadragethe5th.*\n");
 
-//* some discalamer about how this just a portal to yt and all that boring siclamer stuff
-printf("locating network...\n");
-checknetwork();
-//wait unitl network found if not after a muinite,go to connection failed and start again
-if(networkfound==1) {
-succsess();
-} else{
-fail();
-return 0;
-}
+    //* some discalamer about how this just a portal to yt and all that boring siclamer stuff
+    printf("locating network...\n");
+    checknetwork();
+    //wait unitl network found if not after a muinite,go to connection failed and start again
+    if (networkfound==1) {
+        
+        success();
+        
+    } else {
+        
+        fail();
+        
+    return 0;
+        
+    }
+    
 }
