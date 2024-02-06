c6e6931e0777f9d2f9cf14c8e532718f06babbae
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-02-06 08:35:03 -0500

    Update bgmanger.c and trying my best know what im doing so dont judge me if im horribly qrong

--- src/bgmanger.c
+++ src/bgmanger.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+int lightmode=1
+//1=light
+//2 eqauls dark
+
+
+void setbglightmode()
+{
+  //make in if statment for chaning th bg color from white to black whene using dark mode.
+  RRLIB_SetBackgroundColour (u8 r, u8 g, u8 b, u8 a)
+}
+int main() {
+setbglightmode()
+}
 
