2fb9f73b34702aa450ed2ee2dca0403ce702e51c
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2024-02-08 11:26:03 -0500

    Color loading moved to main()

--- main.c
+++ main.c
@@ -39,26 +39,6 @@
 ir_t ir;
 
 
-// I vote that we would move these to a header.
-
-#define GRRLIB_BLACK   0x000000FF 
-#define GRRLIB_MAROON  0x800000FF
-#define GRRLIB_GREEN   0x008000FF
-#define GRRLIB_OLIVE   0x808000FF
-#define GRRLIB_NAVY    0x000080FF
-#define GRRLIB_PURPLE  0x800080FF
-#define GRRLIB_TEAL    0x008080FF
-#define GRRLIB_GRAY    0x808080FF
-#define GRRLIB_SILVER  0xC0C0C0FF
-#define GRRLIB_RED     0xFF0000FF
-#define GRRLIB_LIME    0x00FF00FF
-#define GRRLIB_YELLOW  0xFFFF00FF
-#define GRRLIB_BLUE    0x0000FFFF
-#define GRRLIB_FUCHSIA 0xFF00FFFF
-#define GRRLIB_AQUA    0x00FFFFFF
-#define GRRLIB_WHITE   0xFFFFFFFF
-
-
 //
 // I have a love hate relationship with this formatting.
 //
@@ -72,6 +52,11 @@ int main() {
 
     init();
     init_Connect();
+
+    GRRLIB_texImg *full_volume    = GRRLIB_LoadPNG("assets/full_volume.png");
+    GRRLIB_texImg *low_volume     = GRRLIB_LoadPNG("assets/low_volume.png");
+    GRRLIB_texImg *medium_volume  = GRRLIB_LoadPNG("assets/medium_volume.png");
+    GRRLIB_texImg *no_volume      = GRRLIB_LoadPNG("assets/no_volume.png");
 
     while 1 {
 
