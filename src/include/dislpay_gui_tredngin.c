3c78fba0e6d78b235ac67c2001d7d545ae777d7c
ToadRage <153384304+toadragethe5th@users.noreply.github.com>
2024-02-29 09:35:03 -0500

    fixed weird syncing iss

--- src/include/dislpay_gui_tredngin.c
+++ src/include/dislpay_gui_tredngin.c
@@ -2,44 +2,61 @@
 #include <stdio.h>
 
 
-void main()
-{
-displayicons()
-}
+/*
+
+    Trending page
+
+    GUINEA7PIG! REMEMBER TO USE HEADERS!
+
+    The 
+
+    int main() {
+        // function
+    }
+
+    is useless here!
+
+*/
 
 void displayicons()
 {
-GRRLIB_LoadTextureFromFile(trending_selected.png)	//grabs the selected version of the trending page
-//code for displaying the selected version of the trending pagen goes here 
-GRRLIB_LoadTextureFromFile(mostpopunselected.png)
-//code for disp;aying
-GRRLIB_LoadTextureFromFile(search_icon.png)
-//code for display icons
+
+    GRRLIB_LoadTextureFromFile(trending_selected.png);	//grabs the selected version of the trending page
+    GRRLIB_LoadTextureFromFile(mostpopunselected.png);
+    GRRLIB_LoadTextureFromFile(search_icon.png);
+    //code for display icons
 
 }
 
 void displaywiitubelogo()
 {
-GRRLIB_LoadTextureFromFile(wii_tube_logo.png)
-//code for displaying
+
+    GRRLIB_LoadTextureFromFile(wii_tube_logo.png);
+
 }
 
 void displayiconborders()
 {
-    //box for trending      //paremeters:x,y, width hieght,color,filled
- int GRRLIB_Rectangle(0,50,100,200,(0,0,0,0),false )
-//box or most veiwed
- int GRRLIB_Rectangle(0,200,100,200, (0,0,0,0),false )
 
+    
+    GRRLIB_Rectangle(0,50,100,200,(0,0,0,0),false);
+    GRRLIB_Rectangle(0,200,100,200, (0,0,0,0),false);
+    GRRLIB_Rectangle(0,400,100,200,(0,0,0,0),false);
 
-//box for srearch
- int GRRLIB_Rectangle(0,400,100,200,(0,0,0,0),false )
 }
 
 void guiborder()
-{  //start x,sTART Y, end x end y,color
-int GRRLIB_Line	(50,50,480,50,(0,0,0,0))
+{
 
-int GRRLIB_Line	(50,50,50,720,(0,0,0,0) )
+    GRRLIB_Line(50,50,480,50,(0,0,0,0));
+    GRRLIB_Line(50,50,50,720,(0,0,0,0));
+
+
+}
+
+void main()
+{
+
+    displayicons();
 
 }
---
+++ src/todo.md
@@ -0,0 +1,13 @@
+# PLANNER
+
+### ABOUT THIS PLANNER:
+This planner is meant to document all of the notes and stuff we need to do. **ALL MEMBERS OF THE TEAM SHOULD USE THIS AS REFERENCE!**
+
+## TODO:
+
+- Convert all of the non-main files with header functions
+- Trending code
+
+## NOTES:
+
+- Always remember the semicolons.
