cb0e9cbbd6c9e3f2c572f511860727c6a7623da2
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-03-04 12:29:01 -0500

    Add files via upload

---
+++ src/include/display_gui_most_pop.c
@@ -0,0 +1,52 @@
+#include <grrlib.h>
+#include <stdio.h>
+//paremeters
+//void 	GRRLIB_DrawImg (x,y , texture, rotation, scaleX,  scaleY,  color)
+
+
+void main()
+{
+displayicons()
+}
+
+void displayicons()
+{
+GRRLIB_LoadTextureFromFile(trending_unselected.png)	//grabs the selected version of the trending page
+
+GRRLIB_DrawImg (150, 450,trending_unselected.png,0,1,1,(0,0,0,0))
+//code for displaying the selected version of the trending pagen goes here 
+GRRLIB_LoadTextureFromFile(mostpopselected.png)
+GRRLIB_DrawImg (150, 650,trending_unselected.png,0,1,1,(0,0,0,0))
+//code for disp;aying
+GRRLIB_LoadTextureFromFile(search_icon.png)
+GRRLIB_DrawImg (150, 250,search_icon.png,0,1,1,(0,0,0,0))
+//code for display icons
+
+}
+
+void displaywiitubelogo()
+{
+GRRLIB_LoadTextureFromFile(wii_tube_logo.png)
+GRRLIB_DrawImg (150,0,wii_tube_logo.pngg,0,1,1,(0,0,0,0))
+//code for displaying
+}
+
+void displayiconborders()
+{
+    //box for trending
+ int GRRLIB_Rectangle(0,50,100,200, (0,0,0,0),false)//paremeters:x,y, width hieght,color,filled
+//box or most veiwed
+ int GRRLIB_Rectangle(0, 200, 100,200, (0,0,0,0),false )
+
+
+//box for srearch
+ int GRRLIB_Rectangle( 0,400,100,200,(0,0,0,0),false )
+}
+
+void guiborder()
+{
+int GRRLIB_Line	(50,50,480,50,(0,0,0,0) )//start x,sTART Y, end x end y,color
+
+int GRRLIB_Line	(50,50,50,720,(0,0,0,0) )
+
+}
