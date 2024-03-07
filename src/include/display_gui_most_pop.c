aa54a05cb1e3e3da2f9f9bfc7221b45d3d8920cc
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-03-07 08:44:17 -0500

    indented it

--- src/include/display_gui_most_pop.c
+++ src/include/display_gui_most_pop.c
@@ -6,30 +6,30 @@
 
 void main()
 {
-displayicons()
+    displayicons()
 }
 
 void displayicons()
 {
-GRRLIB_DrawImg (150, 450,trending_unselected.png,0,1,1,(0,0,0,0))
-GRRLIB_DrawImg (150, 650,trending_unselected.png,0,1,1,(0,0,0,0))
-GRRLIB_DrawImg (150, 250,search_icon.png,0,1,1,(0,0,0,0))
+    GRRLIB_DrawImg (150, 450,trending_unselected.png,0,1,1,(0,0,0,0))
+    GRRLIB_DrawImg (150, 650,trending_unselected.png,0,1,1,(0,0,0,0))
+    GRRLIB_DrawImg (150, 250,search_icon.png,0,1,1,(0,0,0,0))
 //code for display icons
 
 }
 
 void displaywiitubelogo()
 {
-GRRLIB_DrawImg (150,0,wii_tube_logo.pngg,0,1,1,(0,0,0,0))
-//code for displaying
+    GRRLIB_DrawImg (150,0,wii_tube_logo.pngg,0,1,1,(0,0,0,0))
+/   /code for displaying
 }
 
 void LoadTextures()
 {
-GRRLIB_LoadTextureFromFile(trending_unselected.png)
-GRRLIB_LoadTextureFromFile(mostpopselected.png)
-GRRLIB_LoadTextureFromFile(search_icon.png)
-GRRLIB_LoadTextureFromFile(wii_tube_logo.png)
+    GRRLIB_LoadTextureFromFile(trending_unselected.png)
+    GRRLIB_LoadTextureFromFile(mostpopselected.png)
+    GRRLIB_LoadTextureFromFile(search_icon.png)
+    GRRLIB_LoadTextureFromFile(wii_tube_logo.png)
 
 
 }
@@ -37,19 +37,17 @@ GRRLIB_LoadTextureFromFile(wii_tube_logo.png)
 void displayiconborders()
 {
     //box for trending
- int GRRLIB_Rectangle(0,50,100,200, (0,0,0,0),false)//paremeters:x,y, width hieght,color,filled
-//box or most veiwed
- int GRRLIB_Rectangle(0, 200, 100,200, (0,0,0,0),false )
-
-
-//box for srearch
- int GRRLIB_Rectangle( 0,400,100,200,(0,0,0,0),false )
+    int GRRLIB_Rectangle(0,50,100,200, (0,0,0,0),false)//paremeters:x,y, width hieght,color,filled
+    //box or most veiwed
+    int GRRLIB_Rectangle(0, 200, 100,200, (0,0,0,0),false )
+    //box for srearch
+    int GRRLIB_Rectangle( 0,400,100,200,(0,0,0,0),false )
 }
 
 void guiborder()
 {
-int GRRLIB_Line	(50,50,480,50,(0,0,0,0) )//start x,sTART Y, end x end y,color
+    int GRRLIB_Line	(50,50,480,50,(0,0,0,0) )//start x,sTART Y, end x end y,color
 
-int GRRLIB_Line	(50,50,50,720,(0,0,0,0) )
+    int GRRLIB_Line	(50,50,50,720,(0,0,0,0) )
 
 }
